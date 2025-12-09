784e0ff144ab068d8751c1efed9dc6caf61fc222
muskaan353 <152877784+muskaan353@users.noreply.github.com>
2025-12-09 19:02:54 +0000

    Conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score = "muskaan"
+
+console.log(typeof score);
+console.log(typeof (score));
+let valueIntNum= Number(score);
+console.log(typeof valueIntNum);
+console.log(valueIntNum);
+
+// "33"= 33;
+// "33abc" = NaN
+// True => 1 , False => 0
+
+let isLoggedIn = "Muskaan";
+let boolisLoggedIn = Boolean(isLoggedIn);
+console.log(boolisLoggedIn);
+
+// 1=>true, 0=> false
+// ""= false
+// "Muskaan"= True
+
+let somenum=33;
+let  stringsomenum= String(somenum);
+console.log(stringsomenum);
+console.log(typeof stringsomenum);
+
+
