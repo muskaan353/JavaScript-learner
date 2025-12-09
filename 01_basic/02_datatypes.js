cbe0b94d7c180334aa8a9d217de59ccd3a15b1e9
muskaan353 <152877784+muskaan353@users.noreply.github.com>
2025-12-09 18:38:02 +0000

    DataTypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; /* treat all js code as newer version */
+// alert(3+3) we are using node js not browser
+
+// console.log(3 
+    // +
+    // 3
+// ) code readability should be high
+let name="Muskaan"
+let age = 22;
+let loggedIn = true;
+
+// number => 2 to power 53;
+// bigInt
+// string => ""
+// boolean => True/False;
+// null => standalone value;
+// undefined =>
+// symbol => unique;
+// object
+
+console.log(typeof age)
+console.log(typeof null) /* object */
